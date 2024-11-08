60c89c2472e1e69c730bc67fcdc405e04b17d3a6
кошька <prirychaudrakonov@gmail.com>
2024-11-08 11:29:46 +0300

    add report

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import hashlib
 import tkinter as tk
 from tkinter import filedialog, messagebox
 
-file_path = None
+file_path= None
 phones = None
 numbers = None
-is_file_loaded = False
+hashes = None
+
 
 
 def salting(phones, numbers):
@@ -18,7 +19,7 @@ def salting(phones, numbers):
         i = 1
         while (str(int(numbers[i]) + salt)) in phones:
             i += 1
-            if i == 5:
+            if i == 2:
                 return salt
     return 0
 def find_salt():
@@ -26,50 +27,79 @@ def find_salt():
     salt = salting(phones, numbers)
     messagebox.showinfo("Готово", f"Значение соли: {salt}")
 
-def load_file():
-    global file_path, is_file_loaded
+def load_file_1():
+    global file_path, numbers, hashes
     file_path = filedialog.askopenfilename()
-    if file_path:
-        is_file_loaded = True
-        button_sha1["state"] = tk.NORMAL
-        button_sha256["state"] = tk.NORMAL
-        button_sha512["state"] = tk.NORMAL
-        button_md5["state"] = tk.NORMAL
-
-
-def hashing(algorithm):
-    global file_path, phones, numbers
     df = pd.read_excel(file_path)
-    hashes = df["Номер телефона"].astype(str)  # Преобразование в строки
+    hashes = df["Номер телефона"].astype(str)
     numbers = [number[:-2] for number in df["Unnamed: 2"].astype(str).tolist()][:5]
+
+
+def load_file_2():
+    global file_path,phones
+    file_path= filedialog.askopenfilename()
+    with open(file_path) as r:
+        phones = [line.strip()[-11:] for line in r.readlines()]
+
+def load_file_3():
+    global file_path,hashes
+    file_path= filedialog.askopenfilename()
+    with open(file_path) as r:
+        hashes = [line.strip() for line in r.readlines()]
+
+def load_file_4():
+    global file_path,phones
+    file_path= filedialog.askopenfilename()
+    with open(file_path) as r:
+        phones = [line.strip() for line in r.readlines()]
+
+
+def deidentify(algorithm):
+    global file_path, phones, numbers,hashes
     alg_index = {'md5': '0', 'sha1': '100', 'sha256': '1400', 'sha512': '1700'}
 
-    # Хеширование с использованием выбранного алгоритма
-    if algorithm == 'md5':
-        hashes = [hashlib.md5(h.encode()).hexdigest() for h in hashes]
-    elif algorithm == 'sha1':
-        hashes = [hashlib.sha1(h.encode()).hexdigest() for h in hashes]
-    elif algorithm == 'sha256':
-        hashes = [hashlib.sha256(h.encode()).hexdigest() for h in hashes]
-    else:
-        hashes = [hashlib.sha512(h.encode()).hexdigest() for h in hashes]
 
-    with open(f'{algorithm}.txt', 'w') as f:
-        for HASH in hashes:
-            f.write(HASH + "\n")
-
-    # Запуск hashcat
-    os.system(
-        f"hashcat -a 3 -m {alg_index[algorithm]} -o output_{algorithm}.txt {algorithm}.txt ?d?d?d?d?d?d?d?d?d?d?d")
+    if algorithm=='md5':
+        with open(fr'{algorithm}.txt', 'w') as f:
+            for HASH in hashes:
+                f.write(HASH + "\n")
+    #Запуск hashcat
+    os.remove('hashcat.potfile')
+    os.system(f"hashcat -a 3 -m {alg_index[algorithm]} -o output_{algorithm}.txt {algorithm}.txt ?d?d?d?d?d?d?d?d?d?d?d")
 
     # Чтение расшифрованных номеров из файла
     with open(fr'C:\hashcat\output_{algorithm}.txt') as r:
         phones = [line.strip()[-11:] for line in r.readlines()]
 
-    with open('phones.txt', 'w') as file:
+    with open(f'phones_{algorithm}.txt', 'w') as file:
         for phone in phones:
             file.write(phone + '\n')
-    messagebox.showinfo("Готово", "Таблица успешно расшифрована. Данные сохранены в файле 'phones.txt'.")
+    messagebox.showinfo("Готово", f"Таблица успешно расшифрована. Данные сохранены в файле 'phones_{algorithm}.txt'.")
+
+
+def encrypt(algorythm):
+    global file_path
+    salt='115'
+    with open(fr'{file_path}') as r:
+        phones = [line.strip()[-11:] for line in r.readlines()]
+    if algorythm=='sha1':
+        phones_sha1 = [hashlib.sha1((phone+salt).encode()).hexdigest() for phone in phones]
+        with open('sha1.txt','w') as f:
+            for HASH in phones_sha1:
+                f.write(HASH + '\n')
+        button_deidentify_sha1['state']=tk.NORMAL
+    if algorythm=='sha256':
+        phones_sha256 = [hashlib.sha256((phone+salt).encode()).hexdigest() for phone in phones]
+        with open('sha256.txt','w') as f:
+            for HASH in phones_sha256:
+                f.write(HASH + '\n')
+        button_deidentify_sha256['state']=tk.NORMAL
+    if algorythm=='sha512':
+        phones_sha512 = [hashlib.sha1((phone+salt).encode()).hexdigest() for phone in phones]
+        with open('sha512.txt','w') as f:
+            for HASH in phones_sha512:
+                f.write(HASH + '\n')
+
 
 
 
@@ -78,20 +108,32 @@ root = tk.Tk()
 root.title("Деобезличивание данных")
 
 
-button_load = tk.Button(root, text="Загрузить", command=load_file)
-button_sha1 = tk.Button(root, text="Деобезличить SHA-1", command=lambda: hashing('sha1'), state=tk.DISABLED)
-button_sha256 = tk.Button(root, text="Деобезличить SHA-256", command=lambda: hashing('sha256'),
-                                     state=tk.DISABLED)
-button_sha512 = tk.Button(root, text="Деобезличить SHA-512", command=lambda: hashing('sha512'),
-                                     state=tk.DISABLED)
-button_md5 = tk.Button(root, text="Деобезличить MD-5", command=lambda: hashing('md5'), state=tk.DISABLED)
+button_load_1 = tk.Button(root, text="Загрузить", command=load_file_1)
+button_deidentify_md5= tk.Button(root, text="Деобезличить MD-5", command=lambda: deidentify("md5"))
 button_compute_salt = tk.Button(root, text="Вычислить соль", command=find_salt)
 
-button_load.grid(row=1, column=0, padx=10, pady=5, sticky="w")
-button_md5.grid(row=2, column=0, padx=10, pady=5, sticky="w")
-button_sha1.grid(row=3, column=0, padx=10, pady=5, sticky="w")
-button_sha256.grid(row=4, column=0, padx=10, pady=5, sticky="w")
-button_sha512.grid(row=5, column=0, padx=10, pady=5, sticky="w")
-button_compute_salt.grid(row=6, column=0, padx=10, pady=5, sticky="w")
+button_load_2=tk.Button(root,text = "Загрузить",command=load_file_2)
+button_encrypt_sha1 = tk.Button(root,text = "Зашифровать SHA-1",command=lambda: encrypt("sha1"))
+button_encrypt_sha256 = tk.Button(root,text = "Зашифровать SHA-256",command=lambda: encrypt("sha256"))
+button_encrypt_sha512 = tk.Button(root,text = "Зашифровать SHA-512",command=lambda: encrypt("sha512"))
+button_deidentify_sha1= tk.Button(root, text="Деобезличить SHA-1", command=lambda: deidentify("sha1"))
+button_deidentify_sha256= tk.Button(root, text="Деобезличить SHA-256", command=lambda: deidentify("sha256"))
+button_deidentify_sha512= tk.Button(root, text="Деобезличить SHA-512", command=lambda: deidentify("sha512"))
+button_load_3=tk.Button(root,text = "Загрузить",command=load_file_3)
+button_load_4=tk.Button(root,text = "Загрузить",command=load_file_4)
+
+button_load_1.grid(row=1, column=0, padx=10, pady=5, sticky="w")
+button_deidentify_md5.grid(row=2, column=0, padx=10, pady=5, sticky="w")
+button_compute_salt.grid(row=4, column=0, padx=10, pady=5, sticky="w")
+button_load_2.grid(row=1, column=2, padx=10, pady=5, sticky="w")
+button_encrypt_sha1.grid(row=2, column=2, padx=10, pady=5, sticky="w")
+button_encrypt_sha256.grid(row=3, column=2, padx=10, pady=5, sticky="w")
+button_encrypt_sha512.grid(row=4, column=2, padx=10, pady=5, sticky="w")
+button_deidentify_sha1.grid(row = 2, column=3,padx=10,pady=5,sticky="w")
+button_deidentify_sha256.grid(row = 3, column=3,padx=10,pady=5,sticky="w")
+button_deidentify_sha512.grid(row = 4, column=3,padx=10,pady=5,sticky="w")
+button_load_3.grid(row=1, column=3, padx=10, pady=5, sticky="w")
+button_load_4.grid(row=3, column=0, padx=10, pady=5, sticky="w")
+
 
 root.mainloop()
